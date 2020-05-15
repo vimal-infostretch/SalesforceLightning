5a050e2967e136a4fc0f676b6adb263c6fdffa73
Vimal Patel <vimal.patel@infostretch.com>
2020-05-15 10:12:58 +0530

    changed

--- force-app/main/default/lwc/sampleLW/basic.js
+++ force-app/main/default/lwc/sampleLW/basic.js
@@ -6,7 +6,7 @@ export default class RadioGroupBasic extends LightningElement {
     get options() {
         return [
             { label: 'Sales', value: 'option1' },
-            { label: 'Force', value: 'option2' },
+            { label: 'Marketing', value: 'option2' },
         ];
     }
 }
