c10702e71c9b13362d175da83feae9002a57a124
Vimal Patel <vimal.patel@infostretch.com>
2020-05-15 12:10:08 +0530

    changed js file

--- force-app/main/default/lwc/sampleLW/sampleLW.js
+++ force-app/main/default/lwc/sampleLW/sampleLW.js
@@ -5,5 +5,26 @@ export default class SampleLW extends LightningElement {
     changeHandler(event) {
       this.greeting = event.target.value;
     }
-    val = 70;
+    val = 85;
+
+    _selected = [];
+
+    get options() {
+        return [
+            { label: 'John', value: 'en' },
+            { label: 'Tim', value: 'de' },
+            { label: 'Greg', value: 'es' },
+            { label: 'Wick', value: 'fr' },
+            { label: 'Tim', value: 'it' },
+            { label: 'Galle', value: 'ja' },
+        ];
+    }
+
+    get selected() {
+        return this._selected.length ? this._selected : 'none';
+    }
+
+    handleChange(e) {
+        this._selected = e.detail.value;
+    }
 }
