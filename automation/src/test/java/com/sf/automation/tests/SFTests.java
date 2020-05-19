e5aa39a9a4cc86cfc19ced0d13cddabbcd9d41e7
Mehul <mehul.kagathara@localhost>
2020-05-19 18:54:06 +0530

    Added second test and removed unused dependancy from pom.xml

--- automation/src/test/java/com/sf/automation/tests/SFTests.java
+++ automation/src/test/java/com/sf/automation/tests/SFTests.java
@@ -4,6 +4,7 @@ import static com.qmetry.qaf.automation.step.CommonStep.click;
 import static com.qmetry.qaf.automation.step.CommonStep.get;
 import static com.qmetry.qaf.automation.step.CommonStep.sendKeys;
 import static com.qmetry.qaf.automation.step.CommonStep.verifyPresent;
+import static com.qmetry.qaf.automation.step.CommonStep.verifyText;
 import static com.qmetry.qaf.automation.step.client.RuntimeScenarioFactory.scenario;
 
 import org.testng.annotations.Test;
@@ -33,28 +34,19 @@ public class SFTests {
 			click("home.q4lead.item2.list");
 			click("home.q4movel2o.button");
 		}).then("verify message", () -> {
-			verifyPresent("home.q4box.label");
+			verifyText("home.q4box.label", "Added values are: Acme corp,Soylent Corp");
 		}).execute();
 	}
-	/*@MetaData("{'TestCaseID':'TC02")
-	@Test(description = "Convert Opportunity to Lead")
+	@MetaData("{'TestCaseID':'TC02")
+	@Test(description = "Convert Opportunity to Lead",dependsOnMethods="ConvertLeadToOpportunity")
 	public void ConvertOpportunityToLead() {
-		scenario().given("user is on application landing page", () -> {
-			get("/");
-			new WebDriverTestBase().getDriver().manage().window().maximize();
-		}).when("user do login to salesforce", () -> {
-			sendKeys(ConfigurationManager.getBundle().getString("username"),"login.username.input");
-			sendKeys(ConfigurationManager.getBundle().getString("password"),"login.password.input");
-			click("login.signin.button");
-		}).then("verify user logged in successfully", () -> {
-			verifyPresent("home.q4pipeline.text");
-		}).when("user moves leads to an opportunity", () -> {
+		scenario().when("user moves an opportunity to lead", () -> {
 			click("home.q4opportunity.item1.list");
 			click("home.q4moveo2l.button");
 			click("home.q4opportunity.item2.list");
 			click("home.q4moveo2l.button");
 		}).then("verify message", () -> {
-			verifyPresent("home.q4box.label");
+			verifyText("home.q4box.label", "Added values are: none");
 		}).execute();
-	}*/
+	}
 }
